b5b54ad670f10f8cc7f640845fdf40c13e1e92da
Sunilkumar <sunilkumarsukumaran222@gmail.com>
2023-03-07 21:13:41 -0600

    First commit

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -2,6 +2,7 @@ package com.springboot.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
